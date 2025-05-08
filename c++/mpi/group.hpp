e52363205be434ab94363401d6b051946183dde5
Thomas Hahn <thomas.hahn01@gmail.com>
2025-05-07 20:06:39 -0400

    Rename member variables in mpi::group

--- c++/mpi/group.hpp
+++ c++/mpi/group.hpp
@@ -38,9 +38,6 @@ namespace mpi {
    * @details It stores an @p MPI_Group object as its only member which by default is set to @p MPI_GROUP_NULL.
    */
   class group {
-    // Wrapped @p MPI_Group object.
-    MPI_Group _grp = MPI_GROUP_NULL;
-
     public:
     /// Construct a group with @p MPI_GROUP_NULL.
     group() = default;
@@ -52,13 +49,13 @@ namespace mpi {
     group &operator=(group const &) = delete;
 
     /// Move constructor leaves moved-from object with @p MPI_GROUP_NULL.
-    group(group &&other) noexcept : _grp{std::exchange(other._grp, MPI_GROUP_NULL)} {}
+    group(group &&other) noexcept : grp_{std::exchange(other.grp_, MPI_GROUP_NULL)} {}
 
     /// Move assignment operator leaves moved-from object with @p MPI_GROUP_NULL.
     group &operator=(group &&rhs) noexcept {
       if (this != std::addressof(rhs)) {
         this->free();
-        this->_grp = std::exchange(rhs._grp, MPI_GROUP_NULL);
+        this->grp_ = std::exchange(rhs.grp_, MPI_GROUP_NULL);
       }
       return *this;
     }
@@ -70,33 +67,33 @@ namespace mpi {
      * @brief Take ownership of an existing @p MPI_Group object.
      * @param grp The group to be handled.
      */
-    explicit group(MPI_Group grp) : _grp(grp) {}
+    explicit group(MPI_Group grp) : grp_(grp) {}
 
     /**
      * @brief Create a group from a communicator.
      * @param c The communicator from which to create a group.
      */
     explicit group(communicator c) {
-      if (has_env) { MPI_Comm_group(c.get(), &_grp); }
+      if (has_env) { MPI_Comm_group(c.get(), &grp_); }
     }
 
     /// Get the wrapped @p MPI_Group object.
-    [[nodiscard]] MPI_Group get() const noexcept { return _grp; }
+    [[nodiscard]] MPI_Group get() const noexcept { return grp_; }
 
     /// Check if the contained @p MPI_Group is @p MPI_GROUP_NULL.
-    [[nodiscard]] bool is_null() const noexcept { return _grp == MPI_GROUP_NULL; }
+    [[nodiscard]] bool is_null() const noexcept { return grp_ == MPI_GROUP_NULL; }
 
     /// Rank of the calling process in the given group.
     [[nodiscard]] int rank() const {
       int rank = 0;
-      if (has_env) { MPI_Group_rank(_grp, &rank); }
+      if (has_env) { MPI_Group_rank(grp_, &rank); }
       return rank;
     }
 
     /// Size of a group.
     [[nodiscard]] int size() const {
       int size = 1;
-      if (has_env) { MPI_Group_size(_grp, &size); }
+      if (has_env) { MPI_Group_size(grp_, &size); }
       return size;
     }
 
@@ -107,16 +104,19 @@ namespace mpi {
      */
     group include(std::vector<int> const &ranks) const {
       MPI_Group newgroup = MPI_GROUP_NULL;
-      if (has_env) { MPI_Group_incl(_grp, ranks.size(), ranks.data(), &newgroup); }
+      if (has_env) { MPI_Group_incl(grp_, ranks.size(), ranks.data(), &newgroup); }
       return group(newgroup);
     }
 
     /// Free the group.
     void free() {
       if (has_env) {
-        if (_grp != MPI_GROUP_NULL) { MPI_Group_free(&_grp); }
+        if (grp_ != MPI_GROUP_NULL) { MPI_Group_free(&grp_); }
       }
     }
+
+    private:
+    MPI_Group grp_ = MPI_GROUP_NULL;
   };
 
 } // namespace mpi
